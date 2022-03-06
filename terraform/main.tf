43cf6429a1ec0d9a7b07fa95bacfd6a72fd3072f
Brian <brian8719@users.noreply.github.com>
2022-03-06 03:39:36 +0000

    Update main.tf
    Flattened nested for_each loop

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,33 @@
-resource "aws_instance" var.INSTANCE_NAME {
+locals {
+  environments = [
+                   "tst",
+                   "stg",
+                   "prd",
+                 ]
+  app_types    = [
+                   "db",
+                   "wa",
+                 ]
+  env_app_types = distinct(flatten([
+    for Env in local.environments : [
+          for App in local.app_types : {
+            Env = Env
+                App = App
+          }
+        ]
+  ]))
+}
+
+resource "aws_instance" "test_demo" {
+  for_each = { for entry in local.env_app_types: "${entry.Env}.${entry.App}" => entry }
+
   ami = lookup(var.AMI, var.REGION)
   instance_type = var.INSTANCE_TYPE
   key_name = var.KEY_NAME
   tags = {
-	Name = var.INSTANCE_NAME
-	Env = var.ENV
-	App = var.APP_TYPE
+        Name = "lnx${each.value.Env}${each.value.App}"
+        Env = each.value.Env
+        App = each.value.App
   }
-  count = var.COUNT
+
 }
