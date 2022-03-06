c6d6f7f06015a31383756ea142f5342aff9dd730
Brian <brian8719@users.noreply.github.com>
2022-03-06 00:01:23 +0000

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,44 @@
+variable "ACCESS_KEY" {}
+variable "SECRET_KEY" {}
+
+variable "AMI" {
+  type = map(string)
+  default = {
+    eu-west-1 = "ami-0bb3fad3c0286ebd5"
+    eu-west-2 = "ami-0a669382ea0feb73a"
+  }
+}
+
+variable "INSTANCE_NAME" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "lnxenvxx000"
+}
+
+variable "ENV" {
+  description = "Environment; Test (tst), Staging (stg) or Production (prd)"
+  type        = string
+  default     = "tst"
+}
+
+variable "APP_TYPE" {
+  description = "Web application (wa) or Database (db)"
+  type        = string
+  default     = "wa"
+}
+
+variable "REGION" {
+  default = "eu-west-1"
+}
+
+variable "INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+
+variable "KEY_NAME" {
+  default = "Generic_Telecoms"
+}
+
+variable "COUNT" {
+  default = "1"
+}
