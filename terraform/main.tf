18eac921583a00cfbb0ea5bffa1a428de7f832ba
Ubuntu <ubuntu@ip-172-31-11-218.eu-west-1.compute.internal>
2022-03-07 19:14:41 +0000

    Test Push from lnxmgmt00

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,42 @@
-resource "aws_instance" var.INSTANCE_NAME {
+locals {
+  environments = [
+                   "tst",
+		   "stg",
+		   "prd",
+		 ]
+  app_types    = [
+                   "db",
+		   "wa",
+                 ]
+  env_app_types = distinct(flatten([
+    for Env in local.environments : [
+	  for App in local.app_types : {
+	    Env = Env
+	    App = App
+	  }
+	]
+  ]))
+}
+
+resource "aws_instance" "test_demo" {
+  for_each = { for entry in local.env_app_types: "${entry.Env}.${entry.App}" => entry }
+
   ami = lookup(var.AMI, var.REGION)
   instance_type = var.INSTANCE_TYPE
   key_name = var.KEY_NAME
   tags = {
-	Name = var.INSTANCE_NAME
-	Env = var.ENV
-	App = var.APP_TYPE
+	Name = "lnx${each.value.Env}${each.value.App}"
+	Env = each.value.Env
+	App = each.value.App
   }
-  count = var.COUNT
+  
+}
+
+resource "aws_s3_bucket" "test_demo" {
+  bucket = "generic-telecoms-s3-bucket-8719"
+}
+
+resource "aws_s3_bucket_acl" "test_demo" {
+  bucket = "generic-telecoms-s3-bucket-8719"
+  acl = "public-read"
 }
